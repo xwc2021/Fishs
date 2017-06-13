a281518ec83474897d79645fb0d631fe814f88c9
xiangwei71 <xiangwei71@gmail.com>
2017-06-14 00:03:53 +0800

    Test Boids in Unity

---
+++ Assets/FishGroup/script/Fish.cs
@@ -0,0 +1,113 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fish : MonoBehaviour {
+
+    static float velocityScale = 10.0f;
+    static float rotaionSpeed = 5.0f;
+
+    static float maxSpeed = 50.0f;
+    static float minSpeed = 2.0f;
+
+    static float cohesionRadius = 10.0f;
+    static float cohesionStrength = 1.0f;
+
+    static float seperateRadius = 5.0f;
+    static float seperateStrength = 5.0f;
+
+    static float guideStrength = 36.0f;
+    static float alignmentStrength = 1.0f;
+    public Guide guide;
+    public Renderer render;
+    public Material materail;
+
+    public Vector3 nowVelocity;
+    public float nowVelocityStrength;
+    Rigidbody rigid;
+    // Use this for initialization
+    void Awake () {
+        rigid = GetComponent<Rigidbody>();
+        materail = render.material;
+    }
+
+    Collider[] gs = new Collider[10];//大小看需求自己設定
+    static float findingGravitySensorR = 5;
+
+    public float cohesionStrengthScale;
+    void calculusVelocity()
+    {
+        Vector3 cohesionVec = (guide.centerPos - transform.position);
+
+        //離中心點遠就遊快一點
+        float D = cohesionVec.magnitude;
+        cohesionStrengthScale = D / cohesionRadius;
+
+        if(cohesionStrengthScale>1)//離太遠就加速
+            cohesionStrengthScale = Mathf.Pow(cohesionStrengthScale, 5);
+
+        cohesionVec = cohesionVec * (cohesionStrengthScale / D);
+
+        Vector3 alignment = guide.averageVeloctiy-nowVelocity;
+
+        int layerMask = 1<<8;
+        int overlapCount = Physics.OverlapSphereNonAlloc(transform.position, findingGravitySensorR, gs, layerMask);
+
+        Vector3 seperateVec = Vector3.zero;
+        for (int i = 0; i < overlapCount; i++)
+        {
+            Transform neighborTransfrom = gs[i].transform;
+            if (neighborTransfrom == transform)
+                continue;
+                
+
+            Vector3 neighborPos = neighborTransfrom.position;
+            Vector3 seperateDiff = transform.position - neighborPos;
+            float d = seperateDiff.magnitude;
+            if (d < seperateRadius)
+            {
+                //離中心愈近，strengthScale愈強
+                float seperateStrengthScale = ( 1.0f - d / seperateRadius)* guideStrength;//和guideStrength成正比
+                seperateVec += seperateDiff * (seperateStrengthScale / d);
+            }
+            
+        }
+
+        Vector3 followTarget = guide.transform.position - transform.position;
+        followTarget.Normalize();
+
+        Vector3 randomVec = new Vector3(Random.value * 2 - 1, Random.value * 2 - 1, Random.value * 2 - 1);
+        Vector3 velocity =
+            randomVec                        //亂數
+            + guideStrength* followTarget    //況目標前進
+            //+ alignmentStrength*alignment   //感覺作用不大  
+            + cohesionStrength * cohesionVec //集中
+            + seperateStrength * seperateVec;//分開
+
+        nowVelocity = velocity * velocityScale*Time.fixedDeltaTime;
+
+        nowVelocityStrength = nowVelocity.magnitude;
+
+        if (nowVelocityStrength > maxSpeed)
+        {
+            nowVelocity = nowVelocity * (maxSpeed / nowVelocityStrength);
+            nowVelocityStrength = maxSpeed;
+        }
+
+        if (nowVelocityStrength < minSpeed)
+        {
+            nowVelocity = nowVelocity * (minSpeed / nowVelocityStrength);
+            nowVelocityStrength = minSpeed;
+        }
+
+        //Debug.DrawLine(transform.position, transform.position + nowVelocity, Color.blue);
+        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(nowVelocity), rotaionSpeed*Time.fixedDeltaTime);
+        rigid.velocity = nowVelocity;
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        calculusVelocity();
+
+    }
+}
---
+++ Assets/FishGroup/script/Guide.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Guide : MonoBehaviour {
+
+    public Vector3 centerPos;
+    public Vector3 averageVeloctiy;
+    public int count=10;
+    public Fish fishPrefab;
+
+    public Bounds bounds;
+    public float minR=2;
+    public float maxR = 10;
+
+    public Color[] colorList;
+
+    public List<Fish> list;
+    // Use this for initialization
+    Vector3 initPos;
+    void Awake () {
+        list = new List<Fish>();
+        for(int i = 0; i < count; i++){
+            Fish fish =GameObject.Instantiate<Fish>(fishPrefab);
+            fish.transform.position = transform.position+Random.Range(minR, maxR) *Random.onUnitSphere;
+            fish.transform.rotation = transform.rotation;
+            fish.guide = this;
+
+            int index = (int)Random.Range(0, colorList.Length);
+            fish.materail.color = colorList[index];
+
+            list.Add(fish);
+        }
+
+        initPos = transform.position;
+
+        StartCoroutine("WaitAndChangePos", 8.0F);
+    }
+
+    float sign = 1;
+    private IEnumerator WaitAndChangePos(float waitTime)
+    {
+        while (true)
+        {
+            transform.position = initPos + sign*transform.forward * 500;
+            sign = sign * -1;
+            yield return new WaitForSeconds(waitTime);
+        }
+    }
+
+
+    // Update is called once per frame
+    void FixedUpdate () {
+
+        Vector3 tempCeterPos = Vector3.zero;
+        Vector3 tempAverageVeloctiy = Vector3.zero;
+        for(int i = 0; i < count; i++)
+        {
+            Fish fish = list[i];
+            tempCeterPos += fish.transform.position;
+            tempAverageVeloctiy += fish.nowVelocity;
+        }
+        centerPos = tempCeterPos / count;
+        averageVeloctiy = tempAverageVeloctiy / count;
+    }
+}
